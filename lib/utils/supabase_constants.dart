d9cf5057ea97db0f0629309f32cb83a2de5f067d
ghoshprince999@gmail.com <ghoshprince999@gmail.com>
2025-11-17 21:30:51 +0600

    add the Project URL, API Key in supabase_contants dart page and edit the main dart page

---
+++ lib/utils/supabase_constants.dart
@@ -0,0 +1,3 @@
+const String appUrl = "https://nkcyapwnukdcxtlphczz.supabase.co";
+const String appKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rY3lhcHdudWtkY3h0bHBoY3p6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzNjAyMzYsImV4cCI6MjA3ODkzNjIzNn0.FToJNDAtBW9i2VA-s9CcG6-GS8FDFfplE6_VlIG4N9U";
