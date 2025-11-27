69d1ffd4f803fd488536cddf77f74cdc66fdb936
ghoshprince999@gmail.com <ghoshprince999@gmail.com>
2025-11-27 11:30:33 +0600

    use user trigger sql for whenever give user input the trigger will add the data into database

---
+++ user_trigger.sql
@@ -0,0 +1,16 @@
+create function public.add_new_user() 
+    returns trigger
+    language plpgsql
+    security definer set search_path = public
+as $$
+begin 
+
+insert into public.users (id, metadata, email)
+    values (new.id, new.raw_user_meta_data, new.email);
+    return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+    after insert on auth.users
+    for each row execute procedure public.add_new_user();
